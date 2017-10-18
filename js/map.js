8be3af6a20b1cdf89359333223f4f671e2ff74e0
Boris Naništa <xnanista@stuba.sk>
2017-10-18 22:15:19 +0200

    init

---
+++ js/map.js
@@ -0,0 +1,11 @@
+function initMap() {
+    var FIIT = {lat: 48.153823, lng: 17.071865};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: FIIT
+    });
+    var marker = new google.maps.Marker({
+        position: FIIT,
+        map: map
+    });
+}
