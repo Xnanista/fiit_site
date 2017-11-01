5b27f4b35149075bd37d27bbe92daf1e60b40fca
Boris Naništa <xnanista@stuba.sk>
2017-11-01 19:24:40 +0100

    youtube_fix

--- js/pluginsConfig_home.js
+++ js/pluginsConfig_home.js
@@ -40,6 +40,12 @@ function tolast() {
     }, 1000);
 };
 
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 var player;
 
 function onYouTubePlayerAPIReady() {
